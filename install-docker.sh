4ccc8fd35580253d9d870dde8b0b6aa94f241f49
Djura Smits <djura.smits@gmail.com>
2020-11-23 13:59:11 +0100

    Fixed curl command

--- install-docker.sh
+++ install-docker.sh
@@ -21,6 +21,6 @@ apt-get install docker-ce docker-ce-cli containerd.io
 
 
 # Install latest stable docker-compose
-curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o
+curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
