57a7a76b9d30b949718af919628390a8c4d8b3e2
Djura Smits <djura.smits@gmail.com>
2020-11-23 14:08:44 +0100

    Create symbolic link docker compose Take postgres user and password frfom .env

--- install-docker.sh
+++ install-docker.sh
@@ -24,3 +24,4 @@ apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
